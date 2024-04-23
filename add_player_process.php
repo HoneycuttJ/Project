8ffc8490fe88e1a53dfb5000e08ebedc961dfbcb
HoneycuttJ <160153601+HoneycuttJ@users.noreply.github.com>
2024-04-23 09:31:53 -0500

    Update add_player_process.php

--- add_player_process.php
+++ add_player_process.php
@@ -10,34 +10,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $height = $_POST['height'];
     $weight = $_POST['weight'];
     $grade = $_POST['grade'];
-    $ppg = $_POST['ppg'];
-    $rpg = $_POST['rpg'];
-    $apg = $_POST['apg'];
-    $spg = $_POST['spg'];
-    $bpg = $_POST['bpg'];
-    $topg = $_POST['topg'];
-    $fg = $_POST['fg'];
-    $threefg = $_POST['3fg'];
-    $ft = $_POST['ft'];
 	$pos = $_POST['pos'];
 
 
     
-	$sql = "INSERT INTO roster_tab (team, name, height, weight, grade, ppg, rpg, apg, spg, bpg, topg, fg, 3fg, ft, position) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	$sql = "INSERT INTO roster_tab (team, name, height, weight, grade, position) 
+            VALUES (?, ?, ?, ?, ?, ?)";
 
 
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssssissssssssss", $team, $name, $height, $weight, $grade, $ppg, $rpg, $apg, $spg, $bpg, $topg, $fg, $threefg, $ft, $pos);
-
-
-    if ($stmt->execute()) {
-
-        echo "Player added successfully!";
-    } else {
-
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    $stmt->bind_param("sssiss", $team, $name, $height, $weight, $grade, $pos);
 
 
     $stmt->close();
