9497940b03d7c50ae441adf05f961e21f55abf55
HoneycuttJ <160153601+HoneycuttJ@users.noreply.github.com>
2024-04-25 10:14:02 -0500

    Update bear.php

--- bear.php
+++ bear.php
@@ -15,7 +15,7 @@ include("db_connection.php");
   <head>
     <title>TIPOFF</title>
   
-    <link rel="stylesheet" href="mystyles.css" />
+    <link rel="stylesheet" href="mystyles2.css" />
 	<script type="text/javascript" src="script.js"></script>
   
   </head>
@@ -25,60 +25,58 @@ include("db_connection.php");
   <body bgcolor="#fff">
     <br><br><br>
   
-    <img src="MainLogo.jpg" alt="TIPOFF Logo" style="width:400px;height:175px;" class="img2">
+   <img src="Basketball.jpg" alt="TIPOFF Logo" style="width:400px;height:200px;" class="img2">
   
   
   
     <ul>
     </ul>
-    <p align="center"> Welcome to TIPOFF - youth sports for the future!
-    <p>
-  
+
   
     <table align="center">
       <tr>
         <td align="center">
           <a href="javascript:void(0)" onclick="popupfunction('1')" class=li>
-            <img src="Bear.jpg" alt="Bear" style="width:200;height:300px" />
+            <img src="Bear1.jpg" alt="Bear" style="width:200;height:300px" />
         </td>
         </a>
         </td>
-      </tr>
-      <tr>
-        <td>The Bears</td>
       </tr>
     </table>
   
   
     <div id="popup1" class="white_content">
     <p align=center><br><br>
-      <img src="teamphoto.jpeg" width=70% /><br><br><br><br>
+	<img src="bearlogo.jpg" width="30%" style="margin-bottom: 0px; margin-top:-20px"/>
+      <img src="teamphoto.jpeg" width=70% /><br>
       <a href="javascript:void(0)" onclick="popupclose('1')" class=linktext>Close Window</a>
       <a href="javascript:void(0)" onclick="popupfunction('4')" class=linktext>Schedule</a>
       <a href="javascript:void(0)" onclick="popupfunction('5')" class=linktext>Stats</a>
 	  <a href="javascript:void(0)" onclick="popupfunction('12')" class=linktext>Roster</a>
 	  <a href="javascript:void(0)" onclick="popupfunction('15')" class=linktext>Add Player</a>	  
-	  <a href="javascript:void(0)" onclick="popupfunction('16')" class=linktext>Edit Team Stats</a>
-	  <a href="javascript:void(0)" onclick="popupfunction('17')" class=linktext>Edit Player Stats</a>	  
+	  <a href="javascript:void(0)" onclick="popupfunction('16')" class=linktext>Edit Team Stats</a><br>
+	  <br><a href="javascript:void(0)" onclick="popupfunction('17')" class=linktext>Edit Player Stats</a>		  
 		  
 
     </p>
   
     </div>
   
-    <div id="popup4" class="white_content">
-         <?php
+ <div id="popup4" class="white_content">
+   <img src="bearlogo.jpg" width="30%" style="margin-bottom: -20px;"/>
+    <?php
 		$sql = "SELECT * FROM schedule_tab WHERE team = 'bears'";
 		$result = $conn->query($sql);
     ?>
-    <p align="center"><br><br>The Bears' Schedule<br><br>
-        <table border="1" style="margin: 0 auto;">
-            <tr>
-                <th>Opponent</th>
-                <th>Game Date</th>
-                <th>Location</th>
-            </tr>
-            <?php
+    <p style="font-family: Arial Black; color: #343551; text-align: center;"><br>Schedule<br></p>
+
+    <table border="1" style="margin: 0 auto;">
+      <tr>
+        <th>Opponent</th>
+        <th>Game Date</th>
+        <th>Location</th>
+      </tr>
+      <?php
             while ($row = $result->fetch_assoc()) {
 				echo '<tr>';
 				echo '<td>' . $row['opp'] . '</td>';
@@ -88,6 +86,7 @@ include("db_connection.php");
 			}
             ?>
     </table>
+	<br>
     <a href="javascript:void(0)" onclick="backToTeam('4')" class="linktext">Back to Team</a>
     </p>
   </div>
@@ -100,19 +99,20 @@ include("db_connection.php");
 		$player_stats_sql = "SELECT name, ppg, rpg, apg, spg, bpg, topg, fg, 3fg, ft FROM roster_tab WHERE team = 'bears'";
 		$player_stats_result = $conn->query($player_stats_sql);
     ?>
-    <p align="center"><br><br>The Bears' Stats<br><br>
-        <table border="1" style="margin: 0 auto;">
-            <tr>
-                <th>PPG</th>
-                <th>OPPG</th>
-                <th>RPG</th>
-                <th>APG</th>
-                <th>FG%</th>
-                <th>3ptFG%</th>
-                <th>FT%</th>
-                <th>Diff.</th>
-            </tr>
-            <?php
+	<img src="bearlogo.jpg" width="30%" style="margin-bottom: -20px;"/>
+    <p style="font-family: Arial Black; color: #343551; text-align: center;"><br>Stats<br></p>
+    <table border="1" style="margin: 0 auto;">
+      <tr>
+        <th>PPG</th>
+        <th>OPPG</th>
+        <th>RPG</th>
+        <th>APG</th>
+        <th>FG%</th>
+        <th>3ptFG%</th>
+        <th>FT%</th>
+        <th>Diff.</th>
+      </tr>
+      <?php
             while ($row = $result->fetch_assoc()) {
 				echo '<tr>';
 				echo '<td>' . $row['ppg'] . '</td>';
@@ -126,23 +126,23 @@ include("db_connection.php");
 				echo '</tr>';
 			}
             ?>
-        </table>
-		<br>
-		<br>
-        <table border="1" style="margin: 0 auto;">
-            <tr>
-                <th>Name</th>
-                <th>PPG</th>
-                <th>RPG</th>
-                <th>APG</th>
-                <th>SPG</th>
-                <th>BPG</th>
-                <th>TO/G</th>
-                <th>FG%</th>
-                <th>3ptFG%</th>
-                <th>FT%</th>
-            </tr>
-            <?php
+    </table>
+    <br>
+    <br>
+    <table border="1" style="margin: 0 auto;">
+      <tr>
+        <th>Name</th>
+        <th>PPG</th>
+        <th>RPG</th>
+        <th>APG</th>
+        <th>SPG</th>
+        <th>BPG</th>
+        <th>TO/G</th>
+        <th>FG%</th>
+        <th>3ptFG%</th>
+        <th>FT%</th>
+      </tr>
+      <?php
             while ($player_row = $player_stats_result->fetch_assoc()) {
                 echo '<tr>';
                 echo '<td>' . $player_row['name'] . '</td>';
@@ -158,26 +158,27 @@ include("db_connection.php");
                 echo '</tr>';
             }
             ?>
-        </table>
-        <a href="javascript:void(0)" onclick="backToTeam('5')" class="linktext">Back to Team</a>
+    </table><br>
+    <a href="javascript:void(0)" onclick="backToTeam('5')" class="linktext">Back to Team</a>
     </p>
-	</div>
-	
-	<div id="popup12" class="white_content">
+  </div>
+
+  <div id="popup12" class="white_content">
     <?php
 		$sql = "SELECT * FROM roster_tab WHERE team = 'bears'";
 		$result = $conn->query($sql);
     ?>
-    <p align="center"><br><br>The Bears' Roster<br><br>
-        <table border="1" style="margin: 0 auto;">
-            <tr>
-                <th>Player</th>
-                <th>Height</th>
-                <th>Weight</th>
-                <th>Grade</th>
-                <th>Position</th>
-            </tr>
-            <?php
+	<img src="bearlogo.jpg" width="30%" style="margin-bottom: -20px;"/>
+    <p style="font-family: Arial Black; color: #343551; text-align: center;"><br>Roster<br>
+    <table border="1" style="margin: 0 auto;">
+      <tr>
+        <th>Player</th>
+        <th>Height</th>
+        <th>Weight</th>
+        <th>Grade</th>
+        <th>Position</th>
+      </tr>
+      <?php
             while ($row = $result->fetch_assoc()) {
 				echo '<tr>';
 				echo '<td>' . $row['name'] . '</td>';
@@ -188,14 +189,14 @@ include("db_connection.php");
 				echo '</tr>';
 			}
             ?>
-        </table>
-        <a href="javascript:void(0)" onclick="backToTeam('12')" class="linktext">Back to Team</a>
+    </table><br>
+    <a href="javascript:void(0)" onclick="backToTeam('12')" class="linktext">Back to Team</a>
     </p>
-	</div>
+  </div>
 	
 <div id="popup15" class="white_content">
     <h1>Add Player</h1>
-     <form id="addPlayerForm" action="add_player_process.php" method="post">
+     <form id="addPlayerForm" method="post">
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
             <input type="text" class="form-control" id="name" name="name" required>
@@ -212,19 +213,188 @@ include("db_connection.php");
             <label for="grade" class="form-label">Grade</label>
             <input type="text" class="form-control" id="grade" name="grade" required>
         </div>
-		<div class="mb-3"
+		<div class="mb-3">
             <label for="postion" class="form-label">Position</label>
             <input type="text" class="form-control" id="pos" name="pos" required>
         </div>
-        <button type="submit" class="linktext">Submit</button>
+        <button type="submit" name="add_player" class="linktext">Submit</button>
 		<a href="javascript:void(0)" onclick="backToTeam('15')" class="linktext">Back to Team</a>
 		<div id="message"></div>
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["add_player"])) {
+
+        $team = $_SESSION['team'];
+        $name = $_POST['name'];
+        $height = $_POST['height'];
+        $weight = $_POST['weight'];
+        $grade = $_POST['grade'];
+        $pos = $_POST['pos'];
+		
+        $sql = "INSERT INTO roster_tab (team, name, height, weight, grade, position) 
+                VALUES (?, ?, ?, ?, ?, ?)";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bind_param("sssiss", $team, $name, $height, $weight, $grade, $pos);
+
+        $stmt->execute();
+
+        $stmt->close();
+
+    }
+}
+?>
+    </form>
+</div>
+<div id="popup16" class="white_content">
+    <h1>Enter New Stats</h1>
+     <form id="editTeamStats" method="post">
+        <div class="mb-3">
+            <label for="name" class="form-label">Points Per Game</label>
+            <input type="text" class="form-control" id="ppg" name="ppg" required>
+        </div>
+        <div class="mb-3">
+            <label for="height" class="form-label">Opponents Points Per Game</label>
+            <input type="text" class="form-control" id="oppg" name="oppg" required>
+        </div>
+        <div class="mb-3">
+            <label for="weight" class="form-label">Rebounds Per Game</label>
+            <input type="text" class="form-control" id="rpg" name="rpg" required>
+        </div>
+        <div class="mb-3">
+            <label for="grade" class="form-label">Assists Per Game</label>
+            <input type="text" class="form-control" id="apg" name="apg" required>
+        </div>
+		<div class="mb-3">
+            <label for="postion" class="form-label">Field Goal%</label>
+            <input type="text" class="form-control" id="fg" name="fg" required>
+        </div>
+		<div class="mb-3">
+            <label for="postion" class="form-label">Three-Point%</label>
+            <input type="text" class="form-control" id="3pt" name="3pt" required>
+        </div>
+		<div class="mb-3">
+            <label for="postion" class="form-label">Free Throw%</label>
+            <input type="text" class="form-control" id="ft" name="ft" required>
+        </div>
+		<div class="mb-3">
+            <label for="postion" class="form-label">Point Differential</label>
+            <input type="text" class="form-control" id="dif" name="dif" required>
+        </div>		
+        <button type="submit" name="editTeamStats" class="linktext">Submit</button>
+		<a href="javascript:void(0)" onclick="backToTeam('16')" class="linktext">Back to Team</a>
+		<div id="message"></div>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["editTeamStats"])) {
+
+        $team = $_SESSION['team'];
+        $ppg = $_POST['ppg'];
+        $oppg = $_POST['oppg'];
+        $rpg = $_POST['rpg'];
+        $apg = $_POST['apg'];
+        $fg = $_POST['fg'];
+        $tpt = $_POST['3pt'];
+        $ft = $_POST['ft'];
+        $dif = $_POST['dif'];
+        
+
+        $sql = "UPDATE stats_tab 
+                SET ppg=?, oppg=?, rpg=?, apg=?, fg=?, 3fg=?, ft=?, diff=? 
+                WHERE team='bears'";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ddddddds", $ppg, $oppg, $rpg, $apg, $fg, $tpt, $ft, $dif);
+
+
+        $stmt->execute();
+
+
+        $stmt->close();
+    }
+?>
+</div>
+<div id="popup17" class="white_content">
+    <h1>Enter Player Stats</h1>
+     <form id="editPlayerStats" method="post">
+        <div class="mb-3">
+            <label for="name" class="form-label">Player Name<label>
+            <input type="text" class="form-control" id="name" name="name" required>
+        </div>
+        <div class="mb-3">
+            <label for="height" class="form-label">Points Per Game</label>
+            <input type="text" class="form-control" id="ppg" name="ppg" required>
+        </div>
+        <div class="mb-3">
+            <label for="weight" class="form-label">Rebounds Per Game</label>
+            <input type="text" class="form-control" id="rpg" name="rpg" required>
+        </div>
+        <div class="mb-3">
+            <label for="grade" class="form-label">Assists Per Game</label>
+            <input type="text" class="form-control" id="apg" name="apg" required>
+        </div>
+		<div class="mb-3">
+            <label for="postion" class="form-label">Steals Per Game</label>
+            <input type="text" class="form-control" id="spg" name="spg" required>
+        </div>
+		<div class="mb-3">
+            <label for="postion" class="form-label">Blocks Per  Game</label>
+            <input type="text" class="form-control" id="bpg" name="bpg" required>
+        </div>
+		<div class="mb-3">
+            <label for="postion" class="form-label">Turnovers Per Game</label>
+            <input type="text" class="form-control" id="topg" name="topg" required>
+        </div>				
+		<div class="mb-3">
+            <label for="postion" class="form-label">Field Goal%</label>
+            <input type="text" class="form-control" id="fg" name="fg" required>
+        </div>
+		<div class="mb-3">
+            <label for="postion" class="form-label">Three-Point%</label>
+            <input type="text" class="form-control" id="3pt" name="3pt" required>
+        </div>
+		<div class="mb-3">
+            <label for="postion" class="form-label">Free Throw%</label>
+            <input type="text" class="form-control" id="ft" name="ft" required>
+        </div>
+        <button type="submit" name="editPlayerStats" class="linktext">Submit</button>
+		<a href="javascript:void(0)" onclick="backToTeam('17')" class="linktext">Back to Team</a>
+		<div id="message"></div>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["editPlayerStats"])) {
+
+        $team = $_SESSION['team'];
+        $name = $_POST['name'];
+        $ppg = $_POST['ppg'];
+        $rpg = $_POST['rpg'];
+        $apg = $_POST['apg'];
+        $spg = $_POST['spg'];
+        $bpg = $_POST['bpg'];
+        $topg = $_POST['topg'];
+        $fg = $_POST['fg'];
+        $tpt = $_POST['3pt'];
+        $ft = $_POST['ft'];
+
+        $sql = "UPDATE roster_tab 
+                SET ppg=?, rpg=?, apg=?, spg=?, bpg=?, topg=?, fg=?, 3fg=?, ft=?
+                WHERE team='bears' AND name=?";
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ddddddddds", $ppg, $rpg, $apg, $spg, $bpg, $topg, $fg, $tpt, $ft, $name);
+
+        $stmt->execute();
+
+        $stmt->close();
+    }
+?>
+
     </form>
 </div>
   <div id="fade2" class="black_content"></div>  
+    <br><br><br><br>
+  <ul>
+    <br>
+    Website By: John Honeycutt and Megan Lavender
+  </ul>
     </center>
   </body>
   </html>
-<?php 
-} 
-?>
